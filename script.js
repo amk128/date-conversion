4c3b65a5705ad029f2b41379f80b6853c3bce5ce
Amir <amk.64.128@gmail.com>
2023-10-16 22:27:41 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// let input1 = document.querySelector('.input');
-// let button1 = document.querySelector('button');
-// let p1 = document.querySelector('p');
+let input1 = document.querySelector('.input');
+let button1 = document.querySelector('button');
+let p1 = document.querySelector('p');
 
-// button1.addEventListener('click' , ()=>{
-//      let d = new persianDate( new Date(input1.value));
-//      d.formatPersian = false;
-//      p1.innerHTML = d.format('YYYY-MM-DD');
-// })
+button1.addEventListener('click' , ()=>{
+     let d = new persianDate( new Date(input1.value));
+     d.formatPersian = false;
+     p1.innerHTML = d.format('YYYY-MM-DD');
+})
 
